345032c4bd0d6feeadb811a9233212da43f385e8
Dukana <dukan091@gmail.com>
2024-10-31 17:59:49 +0800

    first

---
+++ src/components/(routes)/donation/view/view-donation.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import PageTitle from "@/components/page-title";
+import ImageContent from "@/components/image-content";
+import info1 from "../../../../public/image/info/info1.png";
+import info2 from "../../../../public/image/info/info2.png";
+import info3 from "../../../../public/image/info/info3.png";
+
+type IProps = {};
+
+const ViewDonation: React.FC<IProps> = ({}) => {
+
+
+  return (
+    <>
+      <PageTitle title={"Мэдээ мэдээлэл"} />
+hiiiiii
+    </>
+  );
+};
+
+export default ViewDonation;
