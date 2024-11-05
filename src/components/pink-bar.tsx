0b163b505ef5116cc58f29eb580908948b1a5598
Dukana <dukan091@gmail.com>
2024-11-05 13:23:48 +0800

    view donation

---
+++ src/components/pink-bar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function PinkBar() {
+  return (
+    <div style={{
+      backgroundColor: 'red',
+      width: '5px',
+      height: '30px',
+      marginTop: '15px',
+    }} />
+  );
+}
+
+export default PinkBar;
